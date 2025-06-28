efe6a393429c6b955c46cd3ac8b9456d7b83c5ff
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-06-28 19:05:41 -0300

    conversor funcionando e debug das saidas

--- projeto_final/projeto_final.vhd
+++ projeto_final/projeto_final.vhd
@@ -3,8 +3,10 @@ USE ieee.std_logic_1164.all ;
 USE ieee.numeric_std.all;
 
 ENTITY projeto_final IS
-	PORT(clock_cnt, clear_cnt, en_cnt, clock_reg: IN BIT;
-	e, g, l:OUT BIT;
+	PORT(clock_cnt, clear_cnt, en_cnt, clock_reg, clear_reg: IN BIT;
+	saida_cnt: OUT std_logic_vector(11 downto 0);
+	saida_cnt_m, saida_cnt_c, saida_cnt_d, saida_cnt_u: OUT std_logic_vector(3 downto 0);
+	l:OUT BIT;
 	cont: OUT INTEGER
 	);
 END;
@@ -17,8 +19,9 @@ ARCHITECTURE behav OF projeto_final IS
 	END COMPONENT;
 	
 	COMPONENT registrador IS
-		PORT(c, i11, i10, i9, i8, i7, i6, i5, i4, i3, i2, i1, i0:IN BIT;
-		q11, q10, q9, q8, q7, q6, q5, q4, q3, q2, q1, q0:OUT BIT);
+		PORT(c, clr_registrador, ld_registrador:IN BIT;
+		i: IN std_logic_vector(11 DOWNTO 0);
+		q: OUT std_logic_vector(11 DOWNTO 0));
 	END COMPONENT;
 	
 	COMPONENT contador IS
@@ -27,13 +30,13 @@ ARCHITECTURE behav OF projeto_final IS
 	END COMPONENT;
 	
 	COMPONENT conversor IS
-	PORT(segundos: IN INTEGER;
-	s_m, s_c, s_d, s_u:OUT INTEGER);
+	PORT(segundos: IN std_logic_vector(11 downto 0);
+	s_m, s_c, s_d, s_u:OUT std_logic_vector(3 downto 0));
 	END COMPONENT;
 	
 	-- sinais auxiliares para processamento do tempo
 	SIGNAL tempo: INTEGER;
-	SIGNAL segundos_m, segundos_c, segundos_d, segundos_u: INTEGER;
+	SIGNAL bits_segundos, bits_segundos_reg: std_logic_vector(11 downto 0);
 	SIGNAL bits_segundos_m, bits_segundos_c, bits_segundos_d, bits_segundos_u: std_logic_vector(3 downto 0);
 	-- auxiliar para comparacao (<3600)
 	SIGNAL menor_3600: BIT;
@@ -41,21 +44,26 @@ ARCHITECTURE behav OF projeto_final IS
 BEGIN
 
 	-- contador
-	u1: contador PORT MAP(clk_cnt=>clock_cnt, cnt=>en_cnt, clr=>clear_cnt, num=>tempo);
+	u1: contador PORT MAP(clk_cnt=>clock_cnt, cnt=>en_cnt, clr_cnt=>clear_cnt, num=>tempo);
 	u2: comparador_magnitude PORT MAP(A=>tempo, B=>3600, A_lt_B=>menor_3600);
+	l<=menor_3600;
+	-- converte o numero inteiro para vetor de bits
+	bits_segundos <= std_logic_vector(to_unsigned(tempo, 12));
 	
-	-- converte para a saida em cada display
-	u3: conversor PORT MAP(segundos=>tempo, s_m=>segundos_m, s_c=>segundos_c, s_d=>segundos_d, s_u=>segundos_u);
 	
-	bits_segundos_m <= std_logic_vector(to_unsigned(segundos_m, 4));
-	bits_segundos_c <= std_logic_vector(to_unsigned(segundos_c, 4));
-	bits_segundos_d <= std_logic_vector(to_unsigned(segundos_d, 4));
-	bits_segundos_u <= std_logic_vector(to_unsigned(segundos_u, 4));
+	-- registrador do segundos
+	u4: registrador PORT MAP(c=>clock_reg, ld_registrador=>menor_3600, clr_registrador=>clear_reg, i=>bits_segundos, q=>bits_segundos_reg);
+	saida_cnt <= bits_segundos_reg;
+	-- separacao em unidade, dezena, centena e milhar
+	u5: conversor PORT MAP(segundos=>bits_segundos_reg, s_m=>bits_segundos_m, s_c=>bits_segundos_c, s_d=>bits_segundos_d, s_u=>bits_segundos_u);
 	
-	-- armazena no registrador
-	u4: registrador PORT MAP(c=>clock_reg, q11=>'0', q10=>'0')
+	-- debug
+	saida_cnt_m <= bits_segundos_m;
+	saida_cnt_c <= bits_segundos_c;
+	saida_cnt_d <= bits_segundos_d;
+	saida_cnt_u <= bits_segundos_u;
 	
-	--
+	-- decoder
 	
 	
 END; 
