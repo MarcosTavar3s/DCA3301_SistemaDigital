d1604f42187ab006dd0a3adaa3678829cafb600f
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-06-28 18:22:56 -0300

    diminuicao do codigo usando vetores de bits e others

--- projeto_final/registrador.vhd
+++ projeto_final/registrador.vhd
@@ -3,28 +3,23 @@ USE ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 ENTITY registrador IS
-	 PORT(c, i11, i10, i9, i8, i7, i6, i5, i4, i3, i2, i1, i0:IN BIT;
-	 q11, q10, q9, q8, q7, q6, q5, q4, q3, q2, q1, q0:OUT BIT);
+	 PORT(c, clr_registrador, ld_registrador:IN BIT;
+	 i : IN std_logic_vector(11 DOWNTO 0);
+	 q : OUT std_logic_vector(11 DOWNTO 0));
 END;
 
 ARCHITECTURE behav OF registrador IS
-	
+SIGNAL reg : std_logic_vector(11 DOWNTO 0);
+
 BEGIN
 	PROCESS(c)
 		BEGIN
-			IF(c'event AND c='1')THEN
-				q11<=i11;
-				q10<=i10;
-				q9<=i9;
-				q8<=i8;
-				q7<=i7;
-				q6<=i6;
-				q5<=i5;
-				q4<=i4;
-				q3<=i3;
-				q2<=i2;
-				q1<=i1;
-				q0<=i0;
+			IF(c'event and c='1')THEN
+				IF clr_registrador = '1' THEN
+					reg <= (OTHERS => '0');
+				ELSIF ld_registrador = '1' THEN
+				reg <= i;
+				END IF;
 			END IF;
 	END PROCESS;
 	
